b3eae8440a3f20b3e966d3f788230c28286f1d8b
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-18 12:59:54 +0600

    Minor design update

--- public/backend/assets/js/point_sale.js
+++ public/backend/assets/js/point_sale.js
@@ -1,29 +1,4 @@
 
-$('.show_confirm').click(function(event){
-    let form = $(this).closest('form');
-    event.preventDefault();
-
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-        if (result.isConfirmed) {
-            form.submit();
-            Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success"
-            });
-        }
-        });
-
-});
-
 // Custom print and number filter option
 
     $(document).ready(function () {
